d3923e74bb666de38dc81dada2b27ab89d950585
PavanShelat <114757146+PavanShelat@users.noreply.github.com>
2022-10-13 18:56:34 +0530

    Create MODULE_1.c

---
+++ MODULE_1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+
+	int main()
+{
+	printf("MY NAME: SHELAT PAVAN KALPESHBHAI\n");
+	printf("MY BIRTHDATE: 29 JAN,2005\n");
+	printf("MY AGE:18\n");
+	printf("MY ADDRESS:B-48,KRISHNADHAM SOC.,OPP.PUSHPAM COMPLEX,ISANPUR,AHMEDABAD\n");
+
+}
